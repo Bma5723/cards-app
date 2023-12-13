edd281ef9c16f31434a3f8ea05ca580deea9ccb5
Bader Alkatheeri <baderalkatheeri@Baders-MacBook-Pro.local>
2023-12-13 21:13:27 +0400

    pic added

---
+++ src/cards-app.js
@@ -0,0 +1,71 @@
+import { LitElement, html, css } from 'lit';
+import "@lrnwebcomponents/accent-card/accent-card.js";
+import "@lrnwebcomponents/meme-maker/meme-maker.js";
+import "singleton-card/src/singleton-card";
+
+class CardsApp extends LitElement {
+  static properties = {
+    header: { type: String },
+  }
+
+  static styles = css`
+  :host {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: flex-start;
+    font-size: calc(10px + 2vmin);
+    color: #1a2b42;
+    max-width: 960px;
+    margin: 0 auto;
+    text-align: center;
+    background-color: var(--cards-app-background-color);
+  }
+
+  main {
+    flex-grow: 1;
+  }
+
+  .logo {
+    margin-top: 36px;
+    animation: app-logo-spin infinite 20s linear;
+  }
+
+  @keyframes app-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .app-footer {
+    font-size: calc(12px + 0.5vmin);
+    align-items: center;
+  }
+
+  .app-footer a {
+    margin-left: 5px;
+  }
+`;
+
+
+  constructor() {
+    super();
+    this.header = 'My app';
+  }
+
+  render() {
+    return html`
+      <main>
+        <meme-maker alt="Cat stalking a small toy" image-url="https://cdn2.thecatapi.com/images/9j5.jpg" top-text="I bring you" bottom-text="the death"></meme-maker>
+        <singleton-card ></singleton-card>
+        <slot/>
+      </main>
+    `;
+  }
+}
+
+customElements.define('cards-app', CardsApp);
